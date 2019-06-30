ce214eccc2d9ca1200fc633d5dbcbcaba85e8bb6
Phillip Chang <phillip-chang@live.com>
2019-06-30 16:11:07 -0700

    Add array to store friend compatibility scores and look for lowest

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -8,14 +8,32 @@ module.exports = function(app){
     app.post("/api/friends", function(req,res){
         friendData.push(req.body);
         var difference;
-        var totalDiff;
-        for(j = 0; j<userData.length; j++){
+        var total;
+        var friendCompat = [];
+        var lowest = [];
+        for(j = 0; j<userData.scores.length; j++){
             userData[j] = parseInt(question[j]); 
 
                 for ( i = 0; i<friendData.scores.length; i++){
                 difference = (question[j] - parseInt(friendData.scores[i])) *-1;
-                totalDiff += difference; 
-                }
+                total+=difference;
+                var friend = {
+                    name: friendData.name,
+                    compatibility = total,
+                };
+                friendCompat.push(friend);
+            }
         }
+        for (d = 0; d<friendCompat.length; d++){
+            for(l=0;l<lowest.length;l++){
+            if (friendCompat.compatibility[d] <= lowest.compatibility[l]){
+              lowest[l] = friendCompat[d];  
+            } 
+            else{
+                lowest.push(friendCompat[d]);
+            } 
+            }
+        }
+        // add logic for closest match for total.
     });
 }
