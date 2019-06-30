53185e845eff8dc137f02e4fd5329e18b6ed5f0a
Phillip Chang <phillip-chang@live.com>
2019-06-30 16:40:38 -0700

    Add array of friends with keys and values

---
+++ app/data/friends.js
@@ -0,0 +1,85 @@
+var friends = [
+    {
+        name: "Woody",
+        photo: "../images/woody.png",
+        scores:[
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+        ]
+    },
+    {
+        name: "Buzz",
+        photo: "../images/buzz.png",
+        scores:[
+            "4",
+            "2",
+            "5",
+            "1",
+            "3",
+            "2",
+            "2",
+            "1",
+            "3",
+            "2"
+        ]
+    },
+    {
+        name: "Bo",
+        photo: "../images/bo.png",
+        scores:[
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+        ]
+    },
+    {
+        name: "Forky",
+        photo: "../images/forky.png",
+        scores:[
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+        ]
+    },
+    
+    {
+        name: "Potato",
+        photo: "../images/potato.png",
+        scores:[
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+        ]
+    },
+];
+
+module.exports = friends;
