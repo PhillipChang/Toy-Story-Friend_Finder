b382abf5cbc0571e7f16381f11a268c660f994e9
Phillip Chang <phillip-chang@live.com>
2019-07-01 21:05:54 -0700

    Change compatibility logic and add friendindex

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,40 +1,35 @@
-var friendData = require("../data/friends");
+var friends = require("../data/friends");
 
 module.exports = function(app){
     app.get("/api/friends", function(req,res){
-        res.json(friendData);
+        res.json(friends);
     });
 
     app.post("/api/friends", function(req,res){
-        friendData.push(req.body);
-        var difference;
-        var total;
-        var friendCompat = [];
-        var lowest = [];
+        var userData = req.body;
+       
+        // Constant to start difference
+        var lowest = 30; 
+        var friendIndex = 0;
         for(j = 0; j<userData.scores.length; j++){
-            userData[j] = parseInt(question[j]); 
-
-                for ( i = 0; i<friendData.length; i++){
-                    for(m = 0; m<friendData[i].scores.length; m++){
-                        difference = (question[j] - parseInt(friendData[i].scores[m])) *-1;
+            userData.scores[j] = parseInt(userData.scores[j]); 
+        console.log("user score:", userData.scores[j]);
+        }
+                for ( i = 0; i<friends.length; i++){
+                    var total = 0;
+                    for(m = 0; m<friends[i].scores.length; m++){
+                        var difference = Math.abs(userData.scores[m] - parseInt(friends[i].scores[m]));
                         total += difference;
-                        var friend = {
-                            name: friendData.name,
-                            compatibility: total,
-                        };
-                        friendCompat.push(friend);
+                    }
+
+                    if (total < lowest) {
+                        friendIndex = i;
+                        lowest = total;
                     }
                 }
-        }   
-        for (d = 0; d<friendCompat.length; d++){
-            for(l=0;l<lowest.length;l++){
-            if (friendCompat[d].compatibility <= lowest[l].compatibility){
-              lowest[l] = friendCompat[d];  
-            } 
-            else{
-                lowest.push(friendCompat[d]);
-            } 
-            }
-        }
+                friends.push(userData);
+                
+                res.send(friends[friendIndex]);
+                console.log(friends[friendIndex]);
     });
 }
